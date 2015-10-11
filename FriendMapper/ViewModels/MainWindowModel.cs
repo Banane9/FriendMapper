333e4a584afa26e4b5dad32036fa8f913fecbb21
Arne Kiesewetter <banane1996@gmx.net>
2015-10-11 21:39:53 +0200

    Add some UI

---
+++ FriendMapper/ViewModels/MainWindowModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace FriendMapper.ViewModels
+{
+    public sealed class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MapMode MapMode
+        {
+            get { return new AerialMode(true); }
+        }
+
+        #region PropertyChanged
+
+        private void onPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null && propertyName != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion PropertyChanged
+    }
+}
