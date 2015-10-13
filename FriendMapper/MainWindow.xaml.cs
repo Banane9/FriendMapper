e224bb89af4023dae64da96060baf180a1e99d1e
Arne Kiesewetter <banane1996@gmx.net>
2015-10-13 19:38:00 +0200

    Make it possible to add friends

--- FriendMapper/MainWindow.xaml.cs
+++ FriendMapper/MainWindow.xaml.cs
@@ -3,7 +3,7 @@ using FriendMapper.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 
@@ -27,25 +27,41 @@ namespace FriendMapper
                 };
         }
 
+        private void addFriendPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var newFriend = new FriendViewModel();
+
+            var friendSettingsWindow = showFriendSettingsWindow(newFriend);
+
+            friendSettingsWindow.Closed += (_, __) => ((MainWindowViewModel)DataContext).Friends.Add(newFriend);
+        }
+
         private void friendMapMarker_MouseDown(object sender, MouseButtonEventArgs e)
         {
             var friend = (FriendViewModel)((ImageAwesome)sender).DataContext;
             friendMap.SetView(new Microsoft.Maps.MapControl.WPF.Location(friend.Latitude, friend.Longitude), friendMap.ZoomLevel);
         }
 
-        private async void friendSettings_MouseDown(object sender, MouseButtonEventArgs e)
+        private void friendSettings_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            showFriendSettingsWindow((FriendViewModel)((ImageAwesome)sender).DataContext);
+        }
+
+        private Window showFriendSettingsWindow(FriendViewModel friend)
         {
             var friendSettingsWindow = new FriendSettingsWindow();
             friendSettingsWindow.Closed += (window, _) => openedWindows.Remove((Window)window);
             openedWindows.Add(friendSettingsWindow);
 
-            friendSettingsWindow.DataContext = (FriendViewModel)((ImageAwesome)sender).DataContext;
+            friendSettingsWindow.DataContext = friend;
             friendSettingsWindow.Show();
 
-            await Task.Delay(10);
+            Thread.Sleep(10);
 
             friendSettingsWindow.Activate();
             friendSettingsWindow.Focus();
+
+            return friendSettingsWindow;
         }
     }
 }
