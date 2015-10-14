a71d45ea521d30f41da5aa9e3033a3e6813a664a
Arne Kiesewetter <banane1996@gmx.net>
2015-10-14 14:54:54 +0200

    Add search design to address in friend settings

--- FriendMapper/ValueConverters/StringToIsNullOrWhitespaceConverter.cs
+++ FriendMapper/ValueConverters/StringToIsNullOrWhitespaceConverter.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Data;
-using System.Windows.Media;
 
 namespace FriendMapper.ValueConverters
 {
-    [ValueConversion(typeof(Color), typeof(Brush))]
-    public class ColorToBrushConverter : IValueConverter
+    [ValueConversion(typeof(string), typeof(bool))]
+    public class StringToIsNullOrWhitespaceConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return new SolidColorBrush((Color)value);
+            return string.IsNullOrWhiteSpace((string)value);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return ((SolidColorBrush)value).Color;
+            throw new NotImplementedException();
         }
     }
 }
