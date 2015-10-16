0311d2a1d1546765754fd2c34b1a67d1226d7563
Arne Kiesewetter <banane1996@gmx.net>
2015-10-16 17:41:44 +0200

    Add getting coordinates based on addresses

--- FriendMapper/FriendSettingsWindow.xaml.cs
+++ FriendMapper/FriendSettingsWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using BingGeocoder;
+using FriendMapper.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Input;
 
 namespace FriendMapper
 {
@@ -10,9 +13,30 @@ namespace FriendMapper
     /// </summary>
     public partial class FriendSettingsWindow : Window
     {
+        private GeoCoder geocoder = new GeoCoder("As0m7iHT5ho1S3B9FjPmqDbeDE5vtatOxL3DzX-eWR-rSntGsLPM8kc3LT2fSAiB", "FriendMapper", System.Globalization.CultureInfo.CurrentUICulture.Name);
+
         public FriendSettingsWindow()
         {
             InitializeComponent();
+        }
+
+        private async void addressSearchButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var friend = (FriendViewModel)DataContext;
+            var address = friend.Address;
+
+            if (!address.Searchable)
+                return;
+
+            var queryResult = await geocoder.GetCoordinate(
+                addressLine: address.Street,
+                locality: address.Town,
+                adminDistrict: address.Region,
+                postalCode: address.Postcode,
+                countryRegion: address.Country);
+
+            friend.Latitude = queryResult.Item1;
+            friend.Longitude = queryResult.Item2;
         }
     }
 }
